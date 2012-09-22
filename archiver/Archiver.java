4ab4eb02e784d6bed8e382f00e961bf2721b6314
khodzha <khodzha.sh@gmail.com>
2012-09-23 01:38:23 +0400

    comments

--- archiver/Archiver.java
+++ archiver/Archiver.java
@@ -3,6 +3,22 @@ import java.util.zip.*;
 import java.io.*;
 
 public class Archiver {
+	public static void printHelp() {
+		System.out.println("This is Archiver - simple archiver/extractor");
+		System.out.println("To archive files: {a|archive} folder1 file1 file2 folder2 file3 archive_name");
+		System.out.println("To extract files: {x|extract} archive_name");
+		System.out.println("To archive files with comments: {c|comment} folder1 folder2 file1 file2 archive_name");
+	}
+
+	private String getComment(String fileName) {
+		Scanner scanIn = new Scanner(System.in);
+		System.out.println("Enter your comment for file: " + fileName);
+		String temp = scanIn.nextLine();
+		scanIn.close();
+		return temp;
+	}
+
+
 	private void extract(String filename) throws IOException {
 		ZipFile file = new ZipFile(filename);
 		ZipEntry entry;
@@ -29,22 +45,31 @@ public class Archiver {
 		System.out.println("Extraction finished");
 	}
 
-	private void archive(String zipFile, String[] files) throws IOException {
+	private void archive(String zipFile, String[] files, boolean withComments) throws IOException {
+
 		byte[] buffer = new byte[100000];
+		String comment;
+		int offset = 0;
+		int readBytes = 0;
+		FileInputStream fileStream;
 		if (!zipFile.endsWith(".zip")) {
 			zipFile += ".zip";
 		}
 		ZipOutputStream zipStream = new ZipOutputStream(new FileOutputStream(zipFile));
 		for(String file : files) {
-			int offset = 0;
-			int readBytes = 0;
-			FileInputStream fileStream = new FileInputStream(file); 
+			offset = 0;
+			readBytes = 0;
+			fileStream = new FileInputStream(file); 
+			ZipEntry entry = new ZipEntry(file);
+			comment = this.getComment(file);
+			entry.setComment(comment);
 			zipStream.putNextEntry(new ZipEntry(file));
 			while ((readBytes = fileStream.read(buffer)) != -1) {
 				zipStream.write(buffer, offset, readBytes);
 				offset += readBytes;
 			}
 			zipStream.closeEntry();
+			fileStream.close();
 		}
 		zipStream.close();
 	}
@@ -52,7 +77,7 @@ public class Archiver {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		if (args.length == 0) {
-			System.out.println("Please supply list of files and archive name");
+			Archiver.printHelp();
 			return;
 		}
 		Archiver a = new Archiver();
@@ -62,7 +87,15 @@ public class Archiver {
 		} else if (args[0].equals("a") || args[0].equals("archive")) {
 			String[] fileList = new String[args.length - 2];
 			System.arraycopy(args, 1, fileList, 0, fileList.length);
-			a.archive(args[args.length - 1], fileList);
+			// archivation
+			a.archive(args[args.length - 1], fileList, false);
+		} else if (args[0].equals("c") || args[0].equals("comment")) {
+			String[] fileList = new String[args.length - 2];
+			System.arraycopy(args, 1, fileList, 0, fileList.length);
+			// archivation with comments
+			a.archive(args[args.length - 1], fileList, true);
+		} else {
+			Archiver.printHelp();
 		}
 	}
 }
