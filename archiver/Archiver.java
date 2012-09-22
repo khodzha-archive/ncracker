2274e1d7adca6bec53333dcf5899c1a8a9cff4e5
khodzha <khodzha.sh@gmail.com>
2012-09-22 23:35:05 +0400

    archivation and extraction

--- archiver/Archiver.java
+++ archiver/Archiver.java
@@ -5,18 +5,48 @@ import java.io.*;
 public class Archiver {
 	private void extract(String filename) throws IOException {
 		ZipFile file = new ZipFile(filename);
-		while (file.entries().hasMoreElements()) {
-			ZipEntry entry = (ZipEntry) file.entries().nextElement();
-			InputStream zipStream = file.getInputStream(entry);
-			FileOutputStream outStream = new FileOutputStream(entry.getName());
-			byte[] buffer = new byte[100000];
+		ZipEntry entry;
+		InputStream zipStream;
+		FileOutputStream outStream;
+		byte[] buffer;
+		Enumeration entries = file.entries();
+		while (entries.hasMoreElements()) {
+			entry = (ZipEntry) entries.nextElement();
+			zipStream = file.getInputStream(entry);
+			outStream = new FileOutputStream(entry.getName());
+			buffer = new byte[100000];
 			int offset = 0;
 			int readBytes = 0;
 			while ((readBytes = zipStream.read(buffer)) != -1) {
 				outStream.write(buffer, offset, readBytes);
 				offset += readBytes;
 			}
+			zipStream.close();
+			outStream.close();
+			System.out.println( "* " + entry.getName() + " extracted");
 		}
+		System.out.println("==============================================================================");
+		System.out.println("Extraction finished");
+	}
+
+	private void archive(String zipFile, String[] files) throws IOException {
+		byte[] buffer = new byte[100000];
+		if (!zipFile.endsWith(".zip")) {
+			zipFile += ".zip";
+		}
+		ZipOutputStream zipStream = new ZipOutputStream(new FileOutputStream(zipFile));
+		for(String file : files) {
+			int offset = 0;
+			int readBytes = 0;
+			FileInputStream fileStream = new FileInputStream(file); 
+			zipStream.putNextEntry(new ZipEntry(file));
+			while ((readBytes = fileStream.read(buffer)) != -1) {
+				zipStream.write(buffer, offset, readBytes);
+				offset += readBytes;
+			}
+			zipStream.closeEntry();
+		}
+		zipStream.close();
 	}
 
 
@@ -29,26 +59,10 @@ public class Archiver {
 		if (args[0].equals("x") || args[0].equals("extract")) {
 			// extraction
 			a.extract(args[1]);
-		} else {
-			byte[] buffer = new byte[100000];
-			String zipFile = args[args.length - 1];
-			if (!zipFile.endsWith(".zip")) {
-				zipFile += ".zip";
-			}
-			ZipOutputStream zipStream = new ZipOutputStream(new FileOutputStream(zipFile));
-			String[] files = Arrays.copyOfRange(args, 0, args.length -1);
-			for(String file : files) {
-				int offset = 0;
-				int readBytes = 0;
-				FileInputStream fileStream = new FileInputStream(file); 
-				zipStream.putNextEntry(new ZipEntry(file));
-				while ((readBytes = fileStream.read(buffer)) != -1) {
-					zipStream.write(buffer, offset, readBytes);
-					offset += readBytes;
-				}
-				zipStream.closeEntry();
-			}
-			zipStream.close();
+		} else if (args[0].equals("a") || args[0].equals("archive")) {
+			String[] fileList = new String[args.length - 2];
+			System.arraycopy(args, 1, fileList, 0, fileList.length);
+			a.archive(args[args.length - 1], fileList);
 		}
 	}
 }
